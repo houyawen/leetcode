117da4563ddb7a66c4f81bada9cfb988fe3e074e
“houyawen” <houyawen@ofo.com>
2018-08-09 21:39:48 +0800

    1.go

--- 1.go
+++ 1.go
@@ -1,14 +1,26 @@
+/*
+https://leetcode-cn.com/problems/two-sum/description/
+
+给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+
+你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+*/
+
 package main
 
 import "fmt"
 
 func main() {
 	//test
-	var test_arr = [...]int{11, 15, 2, 1, 3, 7, 4, 18}
+	var test_arr = []int{11, 15, 2, 1, 7, 3, 4, 18}
 	target := 9
-
-	var first_key int
-	var second_key int
 
 	first_key, second_key := twoNum(test_arr, target)
 
@@ -17,16 +29,17 @@ func main() {
 	fmt.Printf("%v", return_data)
 }
 
-func twoNum(arr, target) (int, int) {
-	return 1, 2
-	// for k1, v1 := range arr {
-	// 	for k2, v1 := range arr {
-	// 		if k1 != k2 {
-	// 			var tmp = v1 + v2
-	// 			if tmp == target {
-	// 				return [2]int{k1, k2}
-	// 			}
-	// 		}
-	// 	}
-	// }
+func twoNum(arr []int, target int) (int, int) {
+	for k1, v1 := range arr {
+		for k2, v2 := range arr {
+			if k1 != k2 {
+				var tmp = v1 + v2
+				if tmp == target {
+					return k1, k2
+				}
+			}
+		}
+	}
+
+	return 0,0
 }
