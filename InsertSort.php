618623da386432c37fe9be87f86ccb6d7e77dec3
houyawen <892714705@qq.com>
2018-09-04 16:17:58 +0800

    fix

--- InsertSort.php
+++ InsertSort.php
@@ -17,7 +17,25 @@ function insertSort($arr){
 	return $arr;
 }
 
+function inserRSort($arr){
+	for ($i = 1; $i < count($arr); $i++) {
+		$insertData = $arr[$i];
+		$j = $i - 1;
+		while ($j >= 0 && ($arr[$j] < $insertData)) {
+			$tmp = $arr[$j];
+			$arr[$j] = $insertData;
+			$arr[$j+1] = $tmp;
+			$j--;
+		}
+	}
+
+	return $arr;
+}
+
 $arr = [12,34,564,65,7,2,55,655,76];
-var_dump(insertSort($arr)) . "\n";
+var_dump(insertSort($arr)) . "\n";//升序
+
+var_dump(inserRSort($arr));//降序
+
 
 ?>
